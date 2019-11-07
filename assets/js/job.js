6c5334245958bfee36ae4b8997488a5d65f5b867
Michael Hrivnak <mhrivnak@gmail.com>
2019-11-06 22:08:38 -0500

    added pagination button generation, searching now displays real results, user address can be saved to local storage and loaded on page load

--- assets/js/job.js
+++ assets/js/job.js
@@ -1,13 +1,13 @@
-var apiKey = "95ee31820ec45dc8bd2ef7279ba150f4";
+var adzunaApiKey = "95ee31820ec45dc8bd2ef7279ba150f4";
 var app_id = "e2d98a7c"
 var page = 1;
 var results_per_page = 20;
 var numPages;
 
 //returns an array of job results to be formatted by the front end
-function getJobs(stringJobTitle,page){
+function getJobs(stringJobTitle,page,callback){
 
-    var queryURL = "http://api.adzuna.com/v1/api/jobs/ca/search/"+page+"?app_id="+app_id+"c&app_key="+apiKey+"&results_per_page="+results_per_page+"&what="+stringJobTitle+"&content-type=application/json&where=toronto";
+    var queryURL = "https://api.adzuna.com/v1/api/jobs/ca/search/"+page+"?app_id="+app_id+"&app_key="+adzunaApiKey+"&results_per_page="+results_per_page+"&what="+stringJobTitle+"&content-type=application/json&where=toronto";
     var siftedResults = [];
     var totalResults;
     $.ajax({
@@ -17,7 +17,7 @@ function getJobs(stringJobTitle,page){
         
         totalResults = response.count;
         numPages = Math.ceil(totalResults/results_per_page);
-        console.log(totalResults);        
+        console.log(totalResults, response);        
 
         response.results.forEach(element => {
 
@@ -25,10 +25,12 @@ function getJobs(stringJobTitle,page){
             var salaryMax = element.salary_max;
             let strSalary = "";
             
-            if (salaryMin === salaryMax){
-                strSalary = salaryMin+"";
+            
+
+            if ((salaryMin > 0 && salaryMax > 0) && ( salaryMin == salaryMax)){
+                strSalary = "$"+salaryMin;
             }else if (salaryMax > salaryMin){
-                strSalary = salaryMin + "-" + salaryMax;
+                strSalary = "$"+salaryMin + " - $" + salaryMax;
             }else {
                 strSalary = "N/A";
             }
@@ -39,6 +41,7 @@ function getJobs(stringJobTitle,page){
             let jobInfo = {
                 JobTitle: element.title,
                 StrSalary: strSalary,
+                JobDescriptionShort: element.description.substring(0,150)+"...",
                 JobDescription: element.description,
                 JobUrl: element.redirect_url,
                 JobContract: element.contract_time,
@@ -51,7 +54,8 @@ function getJobs(stringJobTitle,page){
             
         });
         console.log(siftedResults);
-        return {results: siftedResults, totalResults: totalResults};
+        callback({results: siftedResults, totalResults: totalResults, pages: numPages});
+        // return {results: siftedResults, totalResults: totalResults};
  
     });
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,47 @@
 
-var jobsDiv = $("jobs")
+var jobsDiv = $("#jobs");
+var SavedAddress;
+var currentPage;
+var currentJob;
 
-var jobsArr = [{}]
+$("#search").on("click",function(){
+    console.log("hi");
+    event.preventDefault();
+    currentPage = 1;
+    currentJob = $("#jobTitle").val().trim();
+    getJobs(currentJob,currentPage,displayJobs);
+});
 
-displayJobs(jobsArr);
+$("#save").on("click",function(){
+    event.preventDefault();
+    localStorage.setItem("SavedAddress",JSON.stringify($("#addressInput").val().trim()));
+});
 
-function displayJobs(jobsArr){
+function init(){
+    SavedAddress = localStorage.getItem("SavedAddress") != null?JSON.parse(localStorage.getItem("SavedAddress")): null;
+    if(SavedAddress != null){
+        $("#addressInput").val(SavedAddress);
+    }
+}
+
+init();
+
+console.log($("#search"));
+
+function displayJobs(jobsObj){
     jobsDiv.empty();
+    let jobsArr = jobsObj.results;
+    
+    let totalResults = jobsObj.totalResults;
+    if (totalResults == 0){
+        jobsDiv.append($("h5").text("Sorry, no results found, please use another search term"));
+        return;
+    }
+    let pages = jobsObj.pages;
+    
+    
     for (var i = 0; i < jobsArr.length;i++){
+        
         
         var cardDiv = $("<div>"); //creating the whole body of the card
         cardDiv.addClass("card");
@@ -22,27 +56,66 @@ function displayJobs(jobsArr){
         cardTitle.html(jobsArr[i].JobTitle);
 
         var cardCompanyName = $("<h6>"); //Company Name
-        cardSubtitle.addClass("card-subtitle mb-2 text-muted");
-        cardSubtitle.html(jobsArr[i].JobMapInfo.CompanyName);
+        cardCompanyName.addClass("card-subtitle mb-2 text-muted");
+        cardCompanyName.html(jobsArr[i].JobMapInfo.CompanyName);
 
         var cardLocation = $("<h6>"); //Location
-        cardSubtitle.addClass("card-subtitle mb-2");
-        cardSubtitle.html(jobsArr[i].JobMapInfo.City);
+        cardLocation.addClass("card-subtitle mb-2 text-muted");
+        cardLocation.html(jobsArr[i].JobMapInfo.City);
+
+        var cardSalary = $("<h6>"); // Salary
+        cardSalary.addClass("card-subtitle mb-2");
+        cardSalary.html(jobsArr[i].StrSalary);
 
         var cardDescription = $("<p>");
         cardDescription.addClass("description");
-        cardDescription.html(jobsArr[i].JobDescription);
+        cardDescription.html(jobsArr[i].JobDescriptionShort);
 
-        var cardBtn = $("<button>") //applying the button with the URL LINK
-        var jobUrlLink = $("<a>").attr("href",jobsArr[i].JobUrl);
-        cardBtn.append(jobUrlLink);
+        // var cardBtn = $("<button>").addClass("btn btn-primary btn-sm"); //applying the button with the URL LINK
+        // var jobUrlLink = $("<a>").attr("href",jobsArr[i].JobUrl).text("Job Link");
+        // cardBtn.append(jobUrlLink);
         
         cardDiv.append(cardBody.append(
-            cardTitle,cardSubtitle,cardCompanyName,cardLocation,cardDescription,cardBtn
+             cardTitle,cardTitle,cardCompanyName,cardLocation,cardSalary,cardDescription
         ))
         
         jobsDiv.append(cardDiv);
     }
+    if (pages > 1){
+        let pageNav = $("<nav>").css("text-align","center");
+        let ul = $("<ul>").addClass("pagination");
+        ul.append($("<li>").addClass("page-item")
+            .append($("<div>").addClass("page-link").text("Previous").on('click',function(){
+            
+            if(currentPage == 1){
+                return;
+            }
+            currentPage--;
+
+            getJobs(currentJob,currentPage,displayJobs);
+        })));
+        for (let i = 1; i <= pages; i++){
+            ul.append($("<li>").addClass("page-item")
+                .append($("<div>").addClass("page-link").text(i).on('click',function(){
+                currentPage = i;
+                getJobs(currentJob,currentPage,displayJobs);
+            })));
+        }
+        ul.append($("<li>").addClass("page-item")
+            .append($("<div>").addClass("page-link").text("Next").on('click',function(){
+            
+            if(currentPage == pages){
+                return;
+            }
+            currentPage++;
+
+            getJobs(currentJob,currentPage,displayJobs);
+        })));
+        
+        jobsDiv.append(pageNav.append(ul));
+        jobsDiv.prepend(pageNav.append(ul));
+    }
+    jobsDiv.prepend($("<h5>").attr("id","resultsCount").css("text-align","center").text(totalResults+" jobs found. (Page "+currentPage+" of " +pages+")" ));
 }
 
 function createFullJobPost(data){
@@ -51,4 +124,6 @@ function createFullJobPost(data){
     //    <div id="map"></div>
     //    <div id="directionsPanel"></div>
     //</div>
+
+    //MapsAPI.initMap(SavedAddress,jobLocation,document.getElementById("map"), document.getElementById("directionsPanel"))
 }
